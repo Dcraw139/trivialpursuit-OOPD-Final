23cae302c6485e7fa5172229a5f0397953968265
Richard Sanchez <superpoptart1@gmail.com>
2018-02-26 13:43:35 -0700

    Setting up player

---
+++ src/main/java/Model/Player.java
@@ -0,0 +1,14 @@
+package Model;
+
+public interface Player {
+
+    public int getInitRoll();
+
+    public int getPosX();
+
+    public void setPosX(int x);
+
+    public int getPosY();
+
+    public void setPosY(int y);
+}
---
+++ src/main/java/Model/PlayerImpl.java
@@ -0,0 +1,112 @@
+package Model;
+
+import java.io.Serializable;
+import java.util.Random;
+
+/**
+ * represents a player
+ */
+public class PlayerImpl extends PersonImpl implements Player, Comparable, Serializable {
+
+    private String name;
+    private int initRoll;
+    private int posX;
+    private int posY;
+    private static final int MIN_POS = 0;
+
+    /**
+     * default constructor
+     */
+    PlayerImpl() {
+        super();
+        Random random = new Random();
+        this.initRoll = random.nextInt(6) + 1;
+    }
+
+    /**
+     * overloaded constructor
+     *
+     * @param n name
+     * @param x x position
+     * @param y y position
+     */
+    PlayerImpl(String n, int x, int y) {
+        super(n);
+        Random random = new Random();
+        this.initRoll = random.nextInt(6) + 1;
+        this.setPosX(x);
+        this.setPosY(y);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    /**
+     * gets the  initial roll
+     *
+     * @return the initial roll
+     */
+    public int getInitRoll() {
+        return initRoll;
+    }
+
+    /**
+     * gets the x position
+     *
+     * @return the x position
+     */
+    public int getPosX() {
+        return posX;
+    }
+
+    /**
+     * sets the X position
+     *
+     * @param posX the position you'd like to set
+     */
+    public void setPosX(int posX) {
+        if (posX < MIN_POS) {
+            throw new IllegalArgumentException("Sorry the x position must be positive");
+        }
+        this.posX = posX;
+    }
+
+    /**
+     * gets the y position
+     *
+     * @return the y position
+     */
+    public int getPosY() {
+        return posY;
+    }
+
+    /**
+     * sets the y position
+     *
+     * @param posY the position you'd like to set
+     */
+    public void setPosY(int posY) {
+        if (posY < MIN_POS) {
+            throw new IllegalArgumentException("Sorry the y position must be positive");
+        }
+        this.posY = posY;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", " + this.getInitRoll() + ", " + this.getPosX() + ", " + this.getPosY();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        PlayerImpl obj = (PlayerImpl) o;
+        return super.compareTo(o);
+    }
+}
