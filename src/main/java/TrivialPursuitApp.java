354ae85842199abea3c3589003a18ee0148536c4
Richard Sanchez <superpoptart1@gmail.com>
2018-02-26 13:23:28 -0700

    Added the running and the startup menu

--- src/main/java/TrivialPursuitApp.java
+++ src/main/java/TrivialPursuitApp.java
@@ -1,4 +1,23 @@
-public class TrivialPursuitApp {
-    private boolean isKillMe = true;
-    private boolean isDumb = true;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TrivialPursuitApp extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(TrivialPursuitApp.class, args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(ClassLoader.getSystemResource("fxml/startup.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Trivial Pursuit");
+        stage.show();
+    }
 }
