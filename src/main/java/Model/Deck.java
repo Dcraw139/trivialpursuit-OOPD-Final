ad7624b6e6ccefcb34da54aeb952a89529eece78
Dcraw139 <DCrawford@student.neumont.edu>
2018-02-28 15:22:36 -0700

    json test implemented to deck

--- src/main/java/Model/Deck.java
+++ src/main/java/Model/Deck.java
@@ -8,6 +8,21 @@ public class Deck extends ArrayList<Card> {
     public void shuffle() {
         Collections.shuffle(this);
     }
+
+    //card
+    //deck name & question list
+    //question, color, category, a1, a2, a3, a4, rA
+
+    public void testJson() {
+    String json = "{\"deckName\" : \"testDeck\" , \"questionList\" : \" "
+            + "[{\"question\" : \"does this work\", \"color\" : \"blue\", \"category\" : \"history\" , "
+            + "\"answer1\" : \"true\" , \"answer2\" : \"false\", \"rightAnswer\" : \"true\"} , "
+            + "{\"question\" : \"does this work\", \"color\" : \"blue\", \"category\" : \"history\" , "
+            + "\"answer1\" : \"true\" , \"answer2\" : \"false\", \"rightAnswer\" : \"true\"} , "
+            + "{\"question\" : \"does this work\", \"color\" : \"blue\", \"category\" : \"history\" , "
+            + "\"answer1\" : \"true\" , \"answer2\" : \"false\", \"rightAnswer\" : \"true\"}]}";
+    }
 }
+
 
 
